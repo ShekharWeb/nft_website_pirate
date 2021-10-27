5d3a3319d20844a43b166d049e364b805d483cf8
Abhishek Thory <abhithory@gmail.com>
2021-10-27 08:57:05 +0530

    Updates

---
+++ precache-manifest.3649d33734f96ad9d7d3de8d2867c6f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "048a38d170154dba3b2a",
+    "url": "/nft_website_pirate/static/css/main.f4052120.chunk.css"
+  },
+  {
+    "revision": "048a38d170154dba3b2a",
+    "url": "/nft_website_pirate/static/js/main.048a38d1.chunk.js"
+  },
+  {
+    "revision": "8c35ca1ee70e3d609e09",
+    "url": "/nft_website_pirate/static/css/1.45b1f592.chunk.css"
+  },
+  {
+    "revision": "8c35ca1ee70e3d609e09",
+    "url": "/nft_website_pirate/static/js/1.8c35ca1e.chunk.js"
+  },
+  {
+    "revision": "cdc0f8224aaa6d919a04",
+    "url": "/nft_website_pirate/static/js/runtime~main.cdc0f822.js"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/nft_website_pirate/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/nft_website_pirate/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "e96142c8261f0750ae097a922b8d3db9",
+    "url": "/nft_website_pirate/static/media/hero2.e96142c8.jpg"
+  },
+  {
+    "revision": "64d5b88a95c5f9fec6139b026bf62c64",
+    "url": "/nft_website_pirate/index.html"
+  }
+];
